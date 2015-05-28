db8bed40bf2e8c532807bfd801da5eeb21d4bd9e
Ray Cheung <ray.swc@gmail.com>
2015-05-29 00:58:42 +0800

    Added a HACK to append orientation if not found
    If the metadata of the file doesn't provide the
    orientation with EXIF, the geometry string is
    broken, then breaks other logics relying on it.

--- lib/paperclip/geometry_detector_factory.rb
+++ lib/paperclip/geometry_detector_factory.rb
@@ -16,7 +16,7 @@ module Paperclip
       begin
         orientation = Paperclip.options[:use_exif_orientation] ?
           "%[exif:orientation]" : "1"
-        Paperclip.run(
+        s = Paperclip.run(
           "identify",
           "-format '%wx%h,#{orientation}' :file", {
             :file => "#{path}[0]"
@@ -24,9 +24,13 @@ module Paperclip
             :swallow_stderr => true
           }
         )
+        # HACK: if the EXIF orientation doesn't parse properly
+        # we the result string would end with ','
+        s = "#{s}1" if s.match(/,$/)
+        s
       rescue Cocaine::ExitStatusError
         ""
-      rescue Cocaine::CommandNotFoundError => e
+      rescue Cocaine::CommandNotFoundError
         raise_because_imagemagick_missing
       end
     end
